ed03a9f56cbe5bdc7d51b55bbb82c7c47d106b9a
apostle <terryinvalid@gmail.com>
2024-08-28 20:09:20 -0700

    server edit

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Get the directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -11,7 +10,8 @@ const app = express();
 // Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
+// Serve index.html for all routes
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
